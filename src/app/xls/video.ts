78bfcf18f34a7e0bc08e0bc0f44914dcb73722da
Zhou Hao <zhou.hao.27@gmail.com>
2017-02-04 12:11:12 +0800

    CMS implemented

--- src/app/xls/video.ts
+++ src/app/xls/video.ts
@@ -5,7 +5,7 @@ export class Video {
   tag: string;
   duration: string;
 
-  // field definitions
+  // yahoo field definitions
   public static readonly FIELD_TITLE = "TITLE";
   public static readonly FIELD_FILENAME = "FileName";
   public static readonly FIELD_DESCRIPTION = "DESCRIPTION";
@@ -17,6 +17,49 @@ export class Video {
 
   // base path
   public static readonly BASE_PATH = "/wowtvvideos/images/";
+
+  // cms field definitions
+  public static readonly CMS_NAME = "Name";
+  public static readonly CMS_DISPLAY_NAME = "Display Name";
+  public static readonly CMS_MOBTITLE = "Mobtitle";
+  public static readonly CMS_RUN_TIME = "Running time";
+  public static readonly CMS_PRICE = "Price";
+  public static readonly CMS_MOBCAST = "MobCast";
+  public static readonly CMS_FORMAT = "Format";
+  public static readonly CMS_SAMSUNG_RUNTIME = "Samsung Runtime";
+  public static readonly CMS_VIDEO_RATE = "Video Rate";
+  public static readonly CMS_RIGHT_DETAILS = "Rights Details";
+  public static readonly CMS_VIDEO_TAG = "Video Tag";
+  public static readonly CMS_SHORT_DESC = "Short Description";
+  public static readonly CMS_DESC = "Description";
+  public static readonly CMS_CAST = "Cast";
+  public static readonly CMS_DIRECTOR = "Director";
+  public static readonly CMS_PROD_YEAR = "Production Year";
+  public static readonly CMS_USER_RATE = "User Rating";
+  public static readonly CMS_LS_IMG = "Landscape Image";
+  public static readonly CMS_THUMB_IMG = "Thumbnail Image1";
+  public static readonly CMS_SMART_TV = "Smart TV";
+  public static readonly CMS_H1_TITLE = "H1 Title";
+  public static readonly CMS_META_KEYWORD = "Meta_Keyword"
+  public static readonly CMS_META_DESC = "Meta_Description";
+  public static readonly CMS_CONTENT_DATE = "V_Content_Date"
+  public static readonly CMS_COUNT = "V_Count";
+  public static readonly CMS_COLORVIDEO = "V_Colorvideo";
+  public static readonly CMS_COUNTRY ="Geo Country";
+  public static readonly CMS_GROUP_DETAILS = "Geo Group Details";
+  public static readonly CMS_WHITELIST_DETAILS = "White List Details"
+  public static readonly CMS_PARTNER = "Video Partner Attach";
+  public static readonly CMS_VIDEO_FILE_NAME = "Video File Name";
+  public static readonly CMS_TRAILER_NAME = "Video Trailer Name";
+  public static readonly CMS_VIDEO_FILE = "video file";
+  public static readonly CMS_TRAILER_FILE = "trailer file";
+  public static readonly CMS_IOS_APP = "iOS apps & video path";
+  public static readonly CMS_IOS_TRAILER = "iOS apps & trailer path";
+  public static readonly CMS_VIDEO_FORMAT = "Video Format Details";
+  public static readonly CMS_YOUTUBE = "YouTube Url";
+  public static readonly CMS_CATEGORY = "Category";
+  public static readonly CMS_PLATFORM = "Platform";
+  public static readonly CMS_ACTIVE = "Active";
 
   private static readonly MON_NAME = [
     "Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
@@ -40,7 +83,61 @@ export class Video {
   }
 
   // convert to a json object cms needed
-  public toCMS(): String {
-    return ''
+  public toCMS(): string {
+    // always only get the first name from tag as a cast
+    let cast = this.tag.split(',')[0];
+    let rate = this.getRandomInt(5, 8);
+
+    return '{'
+      + '"' + Video.CMS_NAME + '":"' + this.title + '",'
+      + '"' + Video.CMS_DISPLAY_NAME + '":"' + this.title + '",'
+      + '"' + Video.CMS_MOBTITLE + '":"' + this.title + '",'
+      + '"' + Video.CMS_RUN_TIME + '":"' + this.duration + '",'
+      + '"' + Video.CMS_PRICE + '":"' + '0.99' + '",'
+      + '"' + Video.CMS_MOBCAST + '":"' + cast + '",'
+      + '"' + Video.CMS_FORMAT + '":"' + "HD" + '",'
+      + '"' + Video.CMS_SAMSUNG_RUNTIME + '":"' + this.duration + '",'
+      + '"' + Video.CMS_VIDEO_RATE + '":"' + rate + '",'
+      + '"' + Video.CMS_RIGHT_DETAILS + '":"' + 'Online,Mobile Browser,Andoird,iOS,Connected TV,Nokia' + '",'
+      + '"' + Video.CMS_VIDEO_TAG + '":"' + '' + '",'
+      + '"' + Video.CMS_SHORT_DESC + '":"' + this.description + '",'
+      + '"' + Video.CMS_DESC + '":"' + this.description + '",'
+      + '"' + Video.CMS_CAST + '":"' + cast + '",'
+      + '"' + Video.CMS_DIRECTOR + '":"' + '' + '",'
+      + '"' + Video.CMS_PROD_YEAR + '":"' +'' + '",'
+      + '"' + Video.CMS_USER_RATE + '":"' + rate + '",'
+      + '"' + Video.CMS_LS_IMG + '":"' + this.filename + '.jpg' + '",'
+      + '"' + Video.CMS_THUMB_IMG + '":"' + this.filename + '.jpg' + '",'
+      + '"' + Video.CMS_SMART_TV + '":"' + this.filename + '.jpg' + '",'
+      + '"' + Video.CMS_H1_TITLE + '":"' + this.title + '",'
+      + '"' + Video.CMS_META_KEYWORD + '":"' + this.tag + '",'
+      + '"' + Video.CMS_META_DESC + '":"' + this.description + '",'
+      + '"' + Video.CMS_CONTENT_DATE + '":"' + '' + '",'
+      + '"' + Video.CMS_COUNT + '":"' + '' + '",'
+      + '"' + Video.CMS_COLORVIDEO + '":"' + '' + '",'
+      + '"' + Video.CMS_COUNTRY + '":"' + '' + '",'
+      + '"' + Video.CMS_GROUP_DETAILS + '":"' + '' + '",'
+      + '"' + Video.CMS_WHITELIST_DETAILS + '":"' + '' + '",'
+      + '"' + Video.CMS_PARTNER + '":"' + 'Splash News & Picture Agency' + '",'
+      + '"' + Video.CMS_VIDEO_FILE_NAME + '":"' + this.filename + '",'
+      + '"' + Video.CMS_TRAILER_NAME + '":"' + '' + '",'
+      + '"' + Video.CMS_VIDEO_FILE + '":"' + '440kbps,1200kbps, 3000kbps' + '",'
+      + '"' + Video.CMS_TRAILER_FILE + '":"' + '' + '",'
+      + '"' + Video.CMS_IOS_APP + '":"' + '64kbps,440kbps,1200kbps' + '",'
+      + '"' + Video.CMS_IOS_TRAILER + '":"' + '' + '",'
+      + '"' + Video.CMS_VIDEO_FORMAT + '":"' + '' + '",'
+      + '"' + Video.CMS_YOUTUBE + '":"' + '' + '",'
+      + '"' + Video.CMS_CATEGORY + '":"' + 'Celebrity News' + '",'
+      + '"' + Video.CMS_PLATFORM + '":"' + 'WF_WP,WF_Sony,WF_Pana,WF_MobBrw,WF_LG,WF_iOS,WF_And,WF_Sam,WF_touch' + '",'
+      + '"' + Video.CMS_ACTIVE + '":"' + '1"'
+    + '}'
+  }
+
+   /**
+   * Returns a random integer between min (inclusive) and max (inclusive)
+   * Using Math.round() will give you a non-uniform distribution!
+   */
+  private getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
--- src/app/xls/xls.component.ts
+++ src/app/xls/xls.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Video } from './video';
 import { LocalDataSource } from 'ng2-smart-table';
-// import * as alasql from 'alasql';
 
 @Component({
   selector: 'app-xls',
@@ -56,16 +55,177 @@ export class XlsComponent implements OnInit {
         sort: false,
         filter: false
       }
-    },
-    attr: {
-      id: "yahooTable"
     }
   };
 
+  // TODO: how to simplified the code
+  // cms table settings
+  private cmsSettings = {
+    hideSubHeader: true,
+    columns: {
+      [Video.CMS_NAME]: {
+        title: Video.CMS_NAME,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_DISPLAY_NAME]: {
+        title: Video.CMS_DISPLAY_NAME,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_MOBTITLE]: {
+        title: Video.CMS_MOBTITLE,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_RUN_TIME]: {
+        title: Video.CMS_MOBTITLE,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_PRICE]: {
+        title: Video.CMS_PRICE,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_MOBCAST]: {
+        title: Video.CMS_MOBCAST,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_FORMAT]: {
+        title: Video.CMS_FORMAT,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_SAMSUNG_RUNTIME]: {
+        title: Video.CMS_SAMSUNG_RUNTIME,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_VIDEO_RATE]: {
+        title: Video.CMS_VIDEO_RATE,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_RIGHT_DETAILS]: {
+        title: Video.CMS_RIGHT_DETAILS,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_VIDEO_TAG]: {
+        title: Video.CMS_VIDEO_TAG,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_SHORT_DESC]: {
+        title: Video.CMS_SHORT_DESC,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_DESC]: {
+        title: Video.CMS_DESC,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_SAMSUNG_RUNTIME]: {
+        title: Video.CMS_SAMSUNG_RUNTIME,
+        sort: false,
+        filter: false
+      },
+      [Video.CMS_CAST]: {
+        title: Video.CMS_CAST
+      },
+      [Video.CMS_DIRECTOR]: {
+        title: Video.CMS_DIRECTOR
+      },
+      [Video.CMS_PROD_YEAR]: {
+        title: Video.CMS_PROD_YEAR
+      },
+      [Video.CMS_USER_RATE]: {
+        title: Video.CMS_USER_RATE
+      },
+      [Video.CMS_LS_IMG]: {
+        title: Video.CMS_LS_IMG
+      },
+      [Video.CMS_THUMB_IMG]: {
+        title: Video.CMS_THUMB_IMG
+      },
+      [Video.CMS_SMART_TV]: {
+        title: Video.CMS_SMART_TV
+      },
+      [Video.CMS_H1_TITLE]: {
+        title: Video.CMS_H1_TITLE
+      },
+      [Video.CMS_META_KEYWORD]: {
+        title: Video.CMS_META_KEYWORD
+      },
+      [Video.CMS_META_DESC]: {
+        title: Video.CMS_META_DESC
+      },
+      [Video.CMS_CONTENT_DATE]: {
+        title: Video.CMS_CONTENT_DATE
+      },
+      [Video.CMS_COUNT]: {
+        title: Video.CMS_COUNT
+      },
+      [Video.CMS_COLORVIDEO]: {
+        title: Video.CMS_COLORVIDEO
+      },
+      [Video.CMS_COUNTRY]: {
+        title: Video.CMS_COUNTRY
+      },
+      [Video.CMS_GROUP_DETAILS]: {
+        title: Video.CMS_GROUP_DETAILS
+      },
+      [Video.CMS_WHITELIST_DETAILS]: {
+        title: Video.CMS_WHITELIST_DETAILS
+      },
+      [Video.CMS_PARTNER]: {
+        title: Video.CMS_PARTNER
+      },
+      [Video.CMS_VIDEO_FILE_NAME]: {
+        title: Video.CMS_VIDEO_FILE_NAME
+      },
+      [Video.CMS_TRAILER_NAME]: {
+        title: Video.CMS_TRAILER_NAME
+      },
+      [Video.CMS_VIDEO_FILE]: {
+        title: Video.CMS_VIDEO_FILE
+      },
+      [Video.CMS_TRAILER_FILE]: {
+        title: Video.CMS_TRAILER_FILE
+      },
+      [Video.CMS_IOS_APP]: {
+        title: Video.CMS_IOS_APP
+      },
+      [Video.CMS_IOS_TRAILER]: {
+        title: Video.CMS_IOS_TRAILER
+      },
+      [Video.CMS_VIDEO_FORMAT]: {
+        title: Video.CMS_VIDEO_FORMAT
+      },
+      [Video.CMS_YOUTUBE]: {
+        title: Video.CMS_YOUTUBE
+      },
+      [Video.CMS_CATEGORY]: {
+        title: Video.CMS_CATEGORY
+      },
+      [Video.CMS_PLATFORM]: {
+        title: Video.CMS_PLATFORM
+      },
+      [Video.CMS_ACTIVE]: {
+        title: Video.CMS_ACTIVE
+      }
+    }
+  }
+
   private yahooData: LocalDataSource;
+  private cmsData: LocalDataSource;
 
   constructor() {
     this.yahooData = new LocalDataSource();
+    this.cmsData = new LocalDataSource();
   }
 
   ngOnInit() {
@@ -73,8 +233,24 @@ export class XlsComponent implements OnInit {
 
   // NOTICE: It will not work if the format changed in splash-video.com
   private update(value: string): void {
-    // this.log += `Text changed to '${value}'\n`
 
+    let video = this.createVideo(value);
+    // append video to videos
+    let str = video.toYahoo()
+
+    this.yahooData.add(JSON.parse(str));
+    this.yahooData.reset();
+
+    let str2 = video.toCMS()
+    // console.log(str2);
+    // console.log(JSON.parse(str2))
+    this.cmsData.add(JSON.parse(str2));
+    this.cmsData.reset();
+
+    this.textValue = '';
+  }
+
+  private createVideo(value: string) : Video {
     var video = new Video();
     var lines = value.split('\n');
     for(var i = 0;i < lines.length;i++){
@@ -100,15 +276,23 @@ export class XlsComponent implements OnInit {
       // to remove splash logo in end of the video should -3 from seconds
       if (key === 'Length of clip (mm:ss)') {
         const data = val.split(':')
-        let m = data[0];
-        let s = parseInt(data[1]) - 3;
-        video.duration = m + ':' + s;
+        var m = parseInt(data[0]);
+        var s = parseInt(data[1])
+        if (s > 3) {
+          s = s - 3;
+        } else {
+          m = m - 1;
+          s = s + 57;
+        }
+        if (m < 0) {
+          m = 0;
+        }
+        video.duration = '00:' + this.leftPad(m,2) + ':' + this.leftPad(s,2);
       }
 
       // from keywords to tag
       if (key === 'Keywords') {
         video.tag = val;
-        // TODO: to get the first one as cast, mobile_cast etc
       }
 
       // from ref to filename
@@ -116,14 +300,7 @@ export class XlsComponent implements OnInit {
         video.filename = val.toLowerCase();
       }
     }
-
-    // append video to videos
-    let str = video.toYahoo()
-
-    this.yahooData.add(JSON.parse(str));
-    this.yahooData.reset();
-
-    this.textValue = '';
+    return video;
   }
 
   private export() {
@@ -131,11 +308,21 @@ export class XlsComponent implements OnInit {
     this.yahooData.getAll().then(
       (data) => {
         if (data.length > 0) {
-          console.log(JSON.stringify(data))
+          //console.log(JSON.stringify(data))
           // this.downloadCSV({filename:'yahoo.csv',data:data});
 
           // save to excel
           this.exportToExcel('yahoo', data)
+        }
+      }
+    )
+  }
+
+  private exportCMS() {
+    this.cmsData.getAll().then(
+      (data) => {
+        if (data.length > 0) {
+          this.exportToExcel('cms',data)
         }
       }
     )
@@ -202,4 +389,11 @@ export class XlsComponent implements OnInit {
     link.click();
   }
 
+  private leftPad(number, targetLength) {
+    var output = number + '';
+    while (output.length < targetLength) {
+        output = '0' + output;
+    }
+    return output;
+  }
 }
